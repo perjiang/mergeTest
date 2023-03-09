690efe07a1fabe9d9f7b4e2e51e57549c1cccf1b
jmr <51981325+perjiang@users.noreply.github.com>
2023-03-09 11:12:04 +0800

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,7 +13,7 @@
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("模拟远程分支版本更快=1");
+        System.out.println("模拟远程分支版本更快=1=2");
         int i = 10;
     }
 }
