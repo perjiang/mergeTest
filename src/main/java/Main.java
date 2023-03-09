e62205f656cb9301a0c24b7b50a54e3b30d4e92f
jiangxiang08 <jiangxing08@meituan.com>
2023-03-09 09:48:11 +0800

    1

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+/**
+ * meituan.com Inc.
+ * Copyright (c) 2010-2023 All Rights Reserved.
+ */
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author jiangxing
+ * @version Main:Main.java v1.0 2023/3/9 09:47 jiangxing Exp $
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
